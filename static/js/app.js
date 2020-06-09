23711875e93115c8399e080e5489d9bb96b3394e
Paolo Vega <pao02esc@gmail.com>
2020-06-08 23:56:41 -0500

    Bubble chart and first draft of needle

--- static/js/app.js
+++ static/js/app.js
@@ -59,6 +59,10 @@ setTimeout(function(){
     // PLot the information
     plotData(samples[0]);
 
+    plotBubbleData(samples[0]);
+
+    plotGaugePlot(samples[0]);
+
 //Wait 3 seconds to execute
 }, 3000);
 
@@ -98,7 +102,8 @@ function plotData(id){
 
 
 function fillDemographics(id){
-    var panel = d3.select(".panel-heading");
+    var panel = d3.select("#contentDemo");
+    panel.html("");
     Object.entries(id).forEach(([key,value]) => {
         if ((typeof value === 'string' || value instanceof String) || 
             (typeof value === 'number' || value instanceof Number))
@@ -111,4 +116,97 @@ function optionChanged(value){
     id = samples.filter((sample) => {return sample.id == value})[0];
     plotData(id);
     fillDemographics(id);
+    plotBubbleData(id);
+}
+
+
+function plotBubbleData(id){
+
+    // Create your trace.
+    trace = {
+        x: id.otu_ids,
+        y: id.sample_values,
+        text: id.otu_labels,
+        mode: 'markers',
+        marker: {
+            size: id.sample_values,
+            color: id.otu_ids
+        }
+    };
+
+    // Create the data array for our plot
+    data = [trace];
+
+    // Define the plot layout
+    layout = {
+        title: `OTUS for ${id.id}`,
+        showlegend: false
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("bubble", data, layout);
+
+}
+
+
+function plotGaugePlot(id){
+
+    var maxWeeks = samples.map((sample) => {return sample.wfreq});
+    maxWeeks = maxWeeks.filter((weeks) => { return weeks != null})
+    maxWeeks = 9;
+    console.log(maxWeeks);
+    var minWeeks = samples.map((sample) => {return sample.wfreq});
+    minWeeks = minWeeks.filter((weeks) => { return weeks != null})
+    minWeeks = 0;
+    console.log(minWeeks);
+
+    labels = ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9',''];
+
+    
+
+    trace1 = {
+        type: "pie",
+        showlegend: false,
+        hole: 0.4,
+        rotation: 90,
+        values: [100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9,100 / 9,100 / 9,100 / 9,100 / 9, 100],
+        text: labels,
+        direction: "clockwise",
+        textinfo: "text",
+        textposition: "inside",
+        marker: {
+          colors: ["#2B2823","#2B2823","#B4C5AC","#2B2823","#7AAE9F","#5CBCAF","#2B2823","#248D8C","#00A2A0","white"]
+        },
+        labels: labels,
+        hoverinfo: "label"
+      };
+      
+      
+      var degrees = 90, radius = .6;
+      var radians = degrees * Math.PI / 180;
+      var x = -1 * radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+      console.log(id.wfreq);
+
+      layout = {
+        shapes:[{
+            type: 'line',
+            x0: 0.5,
+            y0: 0.5,
+            x1: 0.3,
+            y1: 0.55,
+            line: {
+              color: 'black',
+              width: 5
+            }
+          }],
+        title: 'Number of Printers Sold in a Week',
+        xaxis: {visible: false, range: [-1, 1]},
+        yaxis: {visible: false, range: [-1, 1]}
+      };
+      
+      data = [trace1];
+      
+      Plotly.plot("gauge", data, layout, {staticPlot: true});
+      
 }
